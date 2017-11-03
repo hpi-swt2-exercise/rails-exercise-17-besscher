c5fe05e060257d18e9ba39ac77ab74df2c2a20a2
Erik Brendel <erik.brendel@student.hpi.de>
2017-11-03 18:11:08 +0100

    Refs #12: show validation error

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,12 +7,17 @@ class AuthorsController < ApplicationController
     @author = Author.find(params[:id])
   end
 
-  def new; end
+  def new
+    @author = Author.new
+  end
 
   def create
     @author = Author.new(params.require(:author).permit(:first_name, :last_name, :homepage))
 
-    @author.save
-    redirect_to @author
+    if @author.save
+      redirect_to @author
+    else
+      render 'new'
+    end
   end
 end
