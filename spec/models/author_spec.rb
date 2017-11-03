4572bc3cba410d2523fefe62d4df07c2b62f295a
Erik Brendel <erik.brendel@student.hpi.de>
2017-11-03 14:23:47 +0100

    Add Author model and fix test

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,12 +1,10 @@
 require 'rails_helper'
 
-describe "Author Object", type: :model do
-
-  it "should create Alan Turing" do
-    author = Author.new('Alan', 'Turing', 'http://wikipedia.org/Alan_Turing')
+describe 'Author Object', type: :model do
+  it 'should create Alan Turing' do
+    author = Author.new(first_name: 'Alan', last_name: 'Turing', homepage: 'http://wikipedia.org/Alan_Turing')
     expect(author.first_name).to eq('Alan')
     expect(author.last_name).to eq('Turing')
     expect(author.homepage).to eq('http://wikipedia.org/Alan_Turing')
   end
-
 end
