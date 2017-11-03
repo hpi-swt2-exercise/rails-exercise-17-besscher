907466cddaceee6e7191261dbe0a66812c57454a
Erik Brendel <erik.brendel@student.hpi.de>
2017-11-03 22:27:19 +0100

    Refs #27: red test

---
+++ spec/features/paper/edit_paper_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe 'Edit paper page', type: :feature do
+  it 'should render without error' do
+    @test_paper = FactoryGirl.create :paper
+    visit edit_paper_path(@test_paper)
+  end
+  it 'should edit with no error' do
+    @test_paper = FactoryGirl.create :author
+    visit edit_paper_path(@test_paper)
+
+    fill_in 'paper_year', with: 12_345
+    find('input[type="submit"]').click
+  end
+end
