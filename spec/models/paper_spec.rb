6e8cdf239ab3c99a0038f06bc99819481f834d59
Erik Brendel <erik.brendel@student.hpi.de>
2017-11-03 21:48:52 +0100

    Refs #21: red test

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -19,4 +19,9 @@ describe 'paper Object', type: :model do
     expect(paper).not_to be_valid
   end
 
+  it 'should not be valid without a year' do
+    paper = Paper.new(id: 42, title: 'COMPUTING MACHINERY AND INTELLIGENCE', venue: 'Mind 49: 433-460')
+    expect(paper).not_to be_valid
+  end
+
 end
