5a543e8e8bcf67b8f87bbcf8e49367bad1f49ee4
Erik Brendel <erik.brendel@student.hpi.de>
2017-11-03 14:27:03 +0100

    red test for full name

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,10 +1,17 @@
 require 'rails_helper'
 
 describe 'Author Object', type: :model do
+
   it 'should create Alan Turing' do
     author = Author.new(first_name: 'Alan', last_name: 'Turing', homepage: 'http://wikipedia.org/Alan_Turing')
     expect(author.first_name).to eq('Alan')
     expect(author.last_name).to eq('Turing')
     expect(author.homepage).to eq('http://wikipedia.org/Alan_Turing')
   end
+
+  it 'should have a full name' do
+    author = Author.new(first_name: 'Alan', last_name: 'Turing', homepage: 'http://wikipedia.org/Alan_Turing')
+    expect(author.name).to eq('Alan Turing')
+  end
+
 end
