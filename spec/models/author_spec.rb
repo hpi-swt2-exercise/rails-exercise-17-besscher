95bbf7ee6060abb9228f25ab078e52d7ceed0291
Erik Brendel <erik.brendel@student.hpi.de>
2017-11-03 13:58:38 +0100

    Red test for Author model

---
+++ spec/models/author_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe "Author Object", type: :model do
+
+  it "should create Alan Turing" do
+    author = Author.new('Alan', 'Turing', 'http://wikipedia.org/Alan_Turing')
+    expect(author.first_name).to eq('Alan')
+    expect(author.last_name).to eq('Turing')
+    expect(author.homepage).to eq('http://wikipedia.org/Alan_Turing')
+  end
+
+end
