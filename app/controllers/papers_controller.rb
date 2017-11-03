725a53c040fa89741848a5c8ad3f07d594e095e0
Erik Brendel <erik.brendel@student.hpi.de>
2017-11-03 22:09:03 +0100

    Refs #23: Paper to DB

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -1,5 +1,24 @@
 class PapersController < ApplicationController
   def new
-    #@paper = Paper.new
+    @paper = Paper.new
   end
+
+  def show
+    @paper = Paper.find(params[:id])
+  end
+
+  def create
+    @paper = Paper.new(paper_params)
+
+    if @paper.save
+      redirect_to @paper
+    else
+      render 'new'
+    end
+  end
+end
+
+private
+def paper_params
+  params.require(:paper).permit(:title, :venue, :year)
 end
