1553b207972bac4790c25c98daea915cffb60205
Erik Brendel <erik.brendel@student.hpi.de>
2017-11-03 15:31:09 +0100

    Add Author to db / show Author

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -2,6 +2,12 @@ class AuthorsController < ApplicationController
   def new
   end
   def create
-    render plain: params[:author].inspect
+    @author = Author.new(params.require(:author).permit(:first_name, :last_name, :homepage))
+
+    @author.save
+    redirect_to @author
+  end
+  def show
+    @author = Author.find(params[:id])
   end
 end
